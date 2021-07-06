4bae4466346ca98ceb16b79fdc66c11de4607afd
leoalmeida95 <leo.almeida.silva@hotmail.com>
2021-07-06 14:28:51 -0300

    criando novos testes e readme

--- Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -9,6 +9,56 @@ namespace Alura.LeilaoOnline.Tests
     public class LeilaoTestes
     {
         [Fact]
+        public void LeilaoComTresClientesVerificandoGanhador()
+        {
+            //Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+            var beltrano = new Interessada("Beltrano", leilao);
+            int maiorLance = 1000;
+
+            leilao.RecebeLance(maria, 700);
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 850);
+            leilao.RecebeLance(fulano, 900);
+            leilao.RecebeLance(beltrano, maiorLance);
+
+            //Act - método sendo testado
+            leilao.TerminaPregao();
+
+            var valorEsperado = maiorLance;
+            var clienteEsperado = beltrano;
+            //Assert
+            Assert.Equal(valorEsperado, leilao.Ganhador.Valor);
+            Assert.Equal(clienteEsperado, leilao.Ganhador.Cliente);
+        }
+
+        [Fact]
+        public void LeilaoComLancesOrdenadosPorValor()
+        {
+            //Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+            int maiorLance = 1000;
+
+            leilao.RecebeLance(maria, 700);
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(fulano, 900);
+            leilao.RecebeLance(maria, maiorLance);
+
+            //Act - método sendo testado
+            leilao.TerminaPregao();
+
+            var valorObtido = leilao.Ganhador.Valor;
+            var valorEsperado = maiorLance;
+
+            //Assert
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
         public void LeilaoComVariosLances()
         {
             //Arrange
---
+++ README.md
@@ -0,0 +1,5 @@
+# Alura Leilão Online
+API NojdeJS com Express e Mongo DB rodando em container Docker
+
+# Testes Automatizados
+Testes com xUnit, com práticas ágeis como TDD e Test Driven Development
