e366ab5ddaf780aea64f0ff2b7d847c7fba2a658
leoalmeida95 <leo.almeida.silva@hotmail.com>
2021-07-05 22:41:49 -0300

    criando solução

---
+++ alura.leilaoOnline.core/alura.leilaoOnline.core/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace alura.leilaoOnline.core
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
