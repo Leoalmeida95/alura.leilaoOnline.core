801efa9a986e61ddc411f8fca747882bccc70a56
leoalmeida95 <leo.almeida.silva@hotmail.com>
2021-07-06 10:23:05 -0300

    criando projeto console e criando caso de lance do leilão

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(maria, 700);
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 1000);
+        }
+    }
+}
