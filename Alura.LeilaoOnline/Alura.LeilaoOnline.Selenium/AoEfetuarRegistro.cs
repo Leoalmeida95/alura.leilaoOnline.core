cf881a9faac76bff5f5dbf6cd4491d1c96579a6b
leoalmeida95 <leo.almeida.silva@hotmail.com>
2021-07-09 18:37:58 -0300

    teste com dados invalidos, utilizando theory e refatorando codigo

--- Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/AoEfetuarRegistro.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/AoEfetuarRegistro.cs
@@ -19,30 +19,60 @@ namespace Alura.LeilaoOnline.Selenium
             driver = _fixture.driver;
         }
 
-        [Fact]
-        public void QuandoInfoValidasDeveIrApaginaAgradecimento()
+        [Theory]
+        [InlineData("Leonardo A.", "leo@mail.com", "123", "123")]
+        [InlineData("Ana Luiza", "ana@mail.com", "academia", "academia")]
+        public void QuandoInfoValidasDeveIrApaginaAgradecimento(string nome, string email, string senha, string confSenha)
         {
             //arrange
             driver.Navigate().GoToUrl("http://localhost:5000/");
-            var inputNome = driver.FindElement(By.Id("Nome"));
-            var inputEmail = driver.FindElement(By.Id("Email"));
-            var inputSenha = driver.FindElement(By.Id("Password"));
-            var inputConfirmSenha = driver.FindElement(By.Id("ConfirmPassword"));
-
-            inputNome.SendKeys("Leonardo A.");
-            inputEmail.SendKeys("leo@mail.com");
-            inputSenha.SendKeys("123");
-            inputConfirmSenha.SendKeys("123");
-
-            var btnRegistro = driver.FindElement(By.Id("btnRegistro"));
+            preencherFormulario(nome, email, senha, confSenha);
 
             //act 
-            btnRegistro.Click();
+            Registrar();
 
             //assert
             var wait = new WebDriverWait(driver, TimeSpan.FromMilliseconds(10000));
             wait.Until(ExpectedConditions.ElementIsVisible(By.Id("obrigado")));
             Assert.Contains("Obrigado", driver.PageSource);
         }
+
+        [Theory]
+        [InlineData("", "ana@mail.com", "academia", "academia")]
+        [InlineData("Leonardo", "leo", "123", "123")]
+        [InlineData("Leonardo", "leo@mail.com", "123", "1234")]
+        public void QuandoInfoInvalidasDeveManterNoRegistro(string nome, string email, string senha, string confSenha)
+        {
+            //arrange
+            driver.Navigate().GoToUrl("http://localhost:5000/");
+            preencherFormulario(nome, email, senha, confSenha);
+
+            //act 
+            Registrar();
+
+            //assert
+            Assert.Contains("section-registro", driver.PageSource);
+        }
+
+        #region métodos privados
+        private void preencherFormulario(string nome, string email, string senha, string confSenha)
+        {
+            var inputNome = driver.FindElement(By.Id("Nome"));
+            var inputEmail = driver.FindElement(By.Id("Email"));
+            var inputSenha = driver.FindElement(By.Id("Password"));
+            var inputConfirmSenha = driver.FindElement(By.Id("ConfirmPassword"));
+
+            inputNome.SendKeys(nome);
+            inputEmail.SendKeys(email);
+            inputSenha.SendKeys(senha);
+            inputConfirmSenha.SendKeys(confSenha);
+        }
+
+        private void Registrar()
+        {
+            var btnRegistro = driver.FindElement(By.Id("btnRegistro"));
+            btnRegistro.Click();
+        }
+        #endregion
     }
 }
