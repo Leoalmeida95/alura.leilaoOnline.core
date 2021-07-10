17d6d9a93801e8b26bd423d6374d6a6b53e18d61
leoalmeida95 <leo.almeida.silva@hotmail.com>
2021-07-10 09:59:12 -0300

    criando testes e PO para pagina de login

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/AoEfetuarLogin.cs
@@ -0,0 +1,51 @@
+﻿using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium
+{
+    [Collection("Chrome Drive")]
+    public class AoEfetuarLogin
+    {
+        private IWebDriver driver;
+        private LoginPO loginPO;
+
+        public AoEfetuarLogin(TestFixture _fixture)
+        {
+            driver = _fixture.driver;
+            loginPO = new LoginPO(driver);
+        }
+
+        [Fact]
+        public void QuandoCredenciaisValidasDeveIrParaDashboard()
+        {
+            //arrange
+            loginPO.Visitar();
+            loginPO.PreencherFormulario("leo@mail.com", "123");
+
+            //act
+            loginPO.SubmeteFormulario();
+
+            //assert
+            Assert.Contains("Dashboard", driver.Title);
+        }
+
+        [Fact]
+        public void QuandoCredenciaisInalidasDeveContinuarLogin()
+        {
+            //arrange
+            loginPO.Visitar();
+            loginPO.PreencherFormulario("leo@mail.com", "123444");
+
+            //act
+            loginPO.SubmeteFormulario();
+
+            //assert
+            Assert.Contains("Login", driver.PageSource);
+        }
+    }
+}
---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/PageObjects/LoginPO.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alura.LeilaoOnline.Selenium.PageObjects
+{
+    public class LoginPO
+    {
+        private IWebDriver _driver;
+        private By byInputLogin;
+        private By byInputSenha;
+        private By byBotaoLogin;
+
+        public LoginPO(IWebDriver driver)
+        {
+            _driver = driver;
+            byInputLogin = By.Id("Login");
+            byInputSenha = By.Id("Password");
+            byBotaoLogin = By.Id("btnLogin");
+        }
+
+        public void Visitar()
+        {
+            _driver.Navigate().GoToUrl("http://localhost:5000/Autenticacao/Login");
+        }
+
+        public void PreencherFormulario(string login, string senha)
+        {
+            _driver.FindElement(byInputLogin).SendKeys(login);
+            _driver.FindElement(byInputSenha).SendKeys(senha);
+        }
+
+        public void SubmeteFormulario()
+        {
+            _driver.FindElement(byBotaoLogin).Submit();
+        }
+    }
+}
