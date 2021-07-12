fbf222054e7f46cc6240cbefa90f53e665591592
leoalmeida95 <leo.almeida.silva@hotmail.com>
2021-07-12 10:15:47 -0300

    Criando testes logout e dashboard po

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/AoEfetuarLogout.cs
@@ -0,0 +1,40 @@
+﻿using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium
+{
+    [Collection("Chrome Drive")]
+    public class AoEfetuarLogout
+    {
+        private IWebDriver driver;
+        private LoginPO loginPO;
+        private DashboardPO dashPO;
+
+        public AoEfetuarLogout(TestFixture _fixture)
+        {
+            driver = _fixture.driver;
+            loginPO = new LoginPO(driver);
+            dashPO = new DashboardPO(driver);
+        }
+
+        [Fact]
+        public void QuandoLoginValiadoDeveIrParaHomeNaoLogada()
+        {
+            //arrange
+            loginPO.Visitar();
+            loginPO.PreencherFormulario("leo@mail.com", "123");
+            loginPO.SubmeteFormulario();
+
+            //act
+            dashPO.EfetuarLogout();
+
+            //assert
+            Assert.Contains("Próximos Leilões", driver.PageSource);
+        }
+    }
+}
---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/PageObjects/DashboardPO.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alura.LeilaoOnline.Selenium.PageObjects
+{
+    public class DashboardPO
+    {
+        private IWebDriver _driver;
+        private By byLogout;
+
+        public DashboardPO(IWebDriver driver)
+        {
+            _driver = driver;
+            byLogout = By.Id("logout");
+        }
+
+        public void EfetuarLogout() 
+        {
+            var logoutLink = _driver.FindElement(byLogout);
+        }
+    }
+}
