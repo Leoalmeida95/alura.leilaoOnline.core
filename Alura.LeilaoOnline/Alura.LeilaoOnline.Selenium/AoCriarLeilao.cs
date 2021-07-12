87b2bbb0395734b17f51dda7ee11647f0b54516c
leoalmeida95 <leo.almeida.silva@hotmail.com>
2021-07-12 16:02:12 -0300

    criando teste de cadastro de leilão

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/AoCriarLeilao.cs
@@ -0,0 +1,44 @@
+﻿using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium
+{
+    [Collection("Chrome Drive")]
+    public class AoCriarLeilao
+    {
+        private IWebDriver driver;
+        private LoginPO loginPO;
+        private LeilaoPO leilaoPO;
+
+        public AoCriarLeilao(TestFixture _fixture)
+        {
+            driver = _fixture.driver;
+            loginPO = new LoginPO(driver);
+            leilaoPO = new LeilaoPO(driver);
+        }
+
+        [Theory]
+        [InlineData("VENDA CARRO", "Novíssimo", "Arte e Pintura", 10000, @"C:\Users\leonardo.silva\Pictures\Screenshots\1.png", "2021-05-20", "2021-05-21")]
+        public void QuandoLoginAdminEInfoValidasDeveCadastrarLeilao(string titulo, string descricao, string categoria, double valor, string imagem, string inicio, string termino)
+        {
+            //arrange
+            loginPO.Visitar();
+            loginPO.PreencherFormulario("leo@mail.com", "123");
+            loginPO.SubmeteFormulario();
+
+            leilaoPO.Visitar();
+            leilaoPO.PreencherFormulario(titulo, descricao, categoria, valor, imagem, Convert.ToDateTime(inicio), Convert.ToDateTime(termino));
+
+            //act
+            leilaoPO.SubmeteFormulario();
+
+            //assert
+            Assert.Contains("Leilões cadastrados", driver.PageSource);
+        }
+    }
+}
---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/PageObjects/LeilaoPO.cs
@@ -0,0 +1,59 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Alura.LeilaoOnline.Selenium.PageObjects
+{
+    public class LeilaoPO
+    {
+        private IWebDriver _driver;
+        private By byInputTitulo;
+        private By byInputDescricao;
+        private By byClasseCategoria;
+        private By byInputValorInicial;
+        private By byInputImagem;
+        private By byInputInicioPregao;
+        private By byInputTerminoPregao;
+        private By byBotaoSalvar;
+        private By btTagSpan;
+
+        public LeilaoPO(IWebDriver driver)
+        {
+            _driver = driver;
+            byInputTitulo = By.Id("Titulo");
+            byInputDescricao = By.Id("Descricao");
+            byClasseCategoria = By.ClassName("select-dropdown");
+            byInputValorInicial = By.Id("ValorInicial");
+            byInputImagem = By.Id("ArquivoImagem");
+            byInputInicioPregao = By.Id("InicioPregao");
+            byInputTerminoPregao = By.Id("TerminoPregao");
+            byBotaoSalvar = By.CssSelector("button[type=submit]");
+            btTagSpan = By.TagName("span");
+        }
+
+        public void Visitar()
+        {
+            _driver.Navigate().GoToUrl("http://localhost:5000/Leiloes/Novo");
+        }
+
+        public void PreencherFormulario(string titulo, string descricao, string categoria, double valor, string imagem, DateTime inicio, DateTime termino)
+        {
+            _driver.FindElement(byInputTitulo).SendKeys(titulo);
+            _driver.FindElement(byInputDescricao).SendKeys(descricao);
+            _driver.FindElement(byInputValorInicial).SendKeys(valor.ToString());
+            _driver.FindElement(byInputImagem).SendKeys(imagem);
+            _driver.FindElement(byInputInicioPregao).SendKeys(inicio.ToString());
+            _driver.FindElement(byInputTerminoPregao).SendKeys(termino.ToString());
+            _driver.FindElement(byClasseCategoria).Click();
+            _driver.FindElements(btTagSpan).Where(s => s.Text == categoria).First().Click();
+        }
+
+        public void SubmeteFormulario()
+        {
+            _driver.FindElement(byBotaoSalvar).Click();
+        }
+    }
+}
