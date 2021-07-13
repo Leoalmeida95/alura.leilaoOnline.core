d1ac1594a0e1bc43ef385dd31574c792bd211e10
leoalmeida95 <leo.almeida.silva@hotmail.com>
2021-07-13 10:03:08 -0300

    criando teste de filtro de pesquisa e iteragindo com select options

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/AoFiltrarLeiloes.cs
@@ -0,0 +1,41 @@
+﻿using Alura.LeilaoOnline.Selenium.Fixtures;
+using Alura.LeilaoOnline.Selenium.PageObjects;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Selenium
+{
+    [Collection("Chrome Drive")]
+    public class AoFiltrarLeiloes
+    {
+        private IWebDriver driver;
+        private LoginPO loginPO;
+        private DashboardPO dashPO;
+
+
+        public AoFiltrarLeiloes(TestFixture _fixture)
+        {
+            driver = _fixture.driver;
+            loginPO = new LoginPO(driver);
+            dashPO = new DashboardPO(driver);
+        }
+
+        [Fact]
+        public void QuandoLoginInteressadaDeveMostrarPainelResultado()
+        {
+            //arrange
+            loginPO.Visitar();
+            loginPO.PreencherFormulario("leo@mail.com", "123");
+            loginPO.SubmeteFormulario();
+
+            //act
+            dashPO.PesquisarLeiloes(new List<string>() { "Arte", "Automóveis" });
+
+            //assert
+            //Assert.Equal(3, leilaoPO.Categorias.Count());
+        }
+    }
+}
--- Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/PageObjects/DashboardPO.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Selenium/PageObjects/DashboardPO.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium.Interactions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 
@@ -12,12 +13,51 @@ namespace Alura.LeilaoOnline.Selenium.PageObjects
         private IWebDriver _driver;
         private By byLogout;
         private By byMeuPerfil;
+        private By bySelectCategorias;
+        private By byInputTermo;
+        private By byInputAndamento;
+        private By byBotaoPesquisar;
 
         public DashboardPO(IWebDriver driver)
         {
             _driver = driver;
             byLogout = By.Id("logout");
             byMeuPerfil = By.Id("meu-perfil");
+            bySelectCategorias = By.ClassName("select-wrapper");
+        }
+
+        public void PesquisarLeiloes(List<string> cateogiras)
+        {
+            var selectWrapper = _driver.FindElement(bySelectCategorias);
+            selectWrapper.Click();
+
+            Thread.Sleep(2000);
+
+            var opcoes = selectWrapper.FindElements(By.CssSelector("li>span"))
+                                        .ToList();
+
+            opcoes.ForEach(o =>
+            {
+                o.Click();
+            });
+
+            Thread.Sleep(2000);
+
+            cateogiras.ForEach(cat =>
+            {
+                opcoes.Where(o=> o.Text.Contains(cat))
+                .ToList()
+                .ForEach(o=> {
+                    o.Click();
+                });
+            });
+
+            Thread.Sleep(2000);
+
+            selectWrapper.FindElement(By.TagName("li"))
+                .SendKeys(Keys.Tab);
+
+            Thread.Sleep(2000);
         }
 
         public void EfetuarLogout() 
