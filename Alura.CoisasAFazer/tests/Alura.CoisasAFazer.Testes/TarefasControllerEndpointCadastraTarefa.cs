437f567b69aefb688d8bd89efe020959f0be5186
Leoalmeida96 <leo.almeida.silva@hotmail.com>
2021-07-21 12:08:29 -0300

    Criando testes para controller. Teste de categoria inválida

---
+++ Alura.CoisasAFazer/tests/Alura.CoisasAFazer.Testes/TarefasControllerEndpointCadastraTarefa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using Alura.CoisasAFazer.WebApp.Controllers;
+using Alura.CoisasAFazer.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
+using Alura.CoisasAFazer.Services.Handlers;
+using Alura.CoisasAFazer.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alura.CoisasAFazer.Testes
+{
+    public class TarefasControllerEndpointCadastraTarefa
+    {
+        [Fact]
+        public void QuandoTarefaComCategoriaInexisatenteDeveRetornar404()
+        {
+            //arrange
+            var mockLog = new Mock<ILogger<CadastraTarefaHandler>>();
+
+            var options = new DbContextOptionsBuilder<DbTarefasContext>()
+                    .UseInMemoryDatabase("DBTarefasContext")
+                    .Options;
+            var context = new DbTarefasContext(options);
+            var repo = new RepositorioTarefa(context);
+
+            var controlador = new TarefasController(repo, mockLog.Object);
+            var model = new CadastraTarefaVM();
+            model.IdCategoria = 20;
+            model.Titulo = "Estudar";
+            model.Prazo = DateTime.Now;
+
+            //act
+            var retorno = controlador.EndpointCadastraTarefa(model);
+
+            //assert
+            Assert.IsType<NotFoundObjectResult>(retorno);
+        }
+    }
+}
